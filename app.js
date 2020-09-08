1cc6f1ac28e09943a086bcc73db25a99398f26ff
Expert <jakubmarcek955@gmail.com>
2020-09-08 21:17:48 +0200

    daco

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require ('express')
 const app = express();
-
+var cors = require('cors')
 const productRoutes = require('./products');
 const orderRoutes = require('./orders');
 const  mongoose = require('mongoose');
 const bodyParser =require('body-parser')
 
+app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
--- products.js
+++ products.js
@@ -1,7 +1,6 @@
 const express = require ('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
 const Product = require('./product');
 
 router.get('/', (req, res, next) =>{
